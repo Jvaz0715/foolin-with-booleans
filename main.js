b48036fe988b2f1ea7dba3ddb8125567a27faede
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-02-19 13:02:02 -0500

    Adds proper functions

--- main.js
+++ main.js
@@ -2,8 +2,61 @@
  * YOUR CODE BELOW! *
  ********************/
 
+ function moreThan5(num) {
+   return num > 5;
+ };
 
+ function isNewTopScore(score, topScore) {
+  return score > topScore;
+ }
 
+function isInDanger(grade) {
+  return grade >= 60 && grade <= 71;
+}
+
+function isCoasting(grade) {
+  return grade >= 72 && grade <= 83;
+}
+
+function isSucceeding(grade) {
+  return grade >= 84 && grade <= 92;
+}
+
+function isFailing(grade) {
+  return grade < 60;
+}
+
+function isAcing(grade) {
+  return grade > 92;
+}
+
+function isStudent(role) {
+  return role === 'student';
+}
+
+function isTeacher(role) {
+  return role === 'teacher';
+}
+
+function isAdmin(role) {
+  return role === 'admin';
+}
+
+function isElementary(role) {
+  return role === 'elementary';
+}
+
+function areDifferentPeople(name1, name2) {
+  return name1 !== name2;
+}
+
+function isMiddleSchoolTeacher(role, gradeLevel) {
+return role === 'teacher' && gradeLevel >= 6 && gradeLevel <=8;
+}
+
+function notAnElementarySchoolAdministrator(schoolLevel, role) {
+  return schoolLevel !== 'elementary' || role !== 'admin';
+}
 
 
 /********************************************************************************************
